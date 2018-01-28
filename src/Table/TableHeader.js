a7c9f9c7693cfcdb0e2763ff305cc9fee3895aaa
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2018-01-28 03:52:20 -0300

    feat(table): add styles and dividers

--- src/Table/TableHeader.js
+++ src/Table/TableHeader.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
-import {Row, Col, TextStrong} from '@indec/react-native-commons';
+import {Divider} from 'react-native-elements';
+import {Col, Row, TextStrong} from '@indec/react-native-commons';
 
 import columnPropType from './columnPropType';
 import styles from './styles';
 
 const TableHeader = ({columns}) => (
-    <Row>
-        {columns.map(column => (
-            <Col key={column.id} style={[styles.col, column.style]}>
-                <TextStrong style={styles.header}>{column.label}</TextStrong>
-            </Col>
-        ))}
-    </Row>
+    <Fragment>
+        <Row style={styles.headerContainer}>
+            {columns.map(column => (
+                <Col key={column.id} style={[styles.col, column.style]}>
+                    <TextStrong style={styles.header}>{column.label}</TextStrong>
+                </Col>
+            ))}
+        </Row>
+        <Divider style={styles.headerDivider}/>
+    </Fragment>
 );
 
 TableHeader.propTypes = {
--- src/Table/TableRow.js
+++ src/Table/TableRow.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-typos */
-import React from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
+import {Divider} from 'react-native-elements';
 import {Row} from '@indec/react-native-commons';
 
 import columnPropType from './columnPropType';
@@ -9,11 +10,14 @@ import datumPropType from './datumPropType';
 import styles from './styles';
 
 const TableRow = ({datum, columns}) => (
-    <Row key={datum._id} style={styles.row}>
-        {columns.map(column => (
-            <TableCell key={column.id} datum={datum} column={column}/>
-        ))}
-    </Row>
+    <Fragment>
+        <Row key={datum._id} style={styles.row}>
+            {columns.map(column => (
+                <TableCell key={column.id} datum={datum} column={column}/>
+            ))}
+        </Row>
+        <Divider style={styles.rowDivider}/>
+    </Fragment>
 );
 
 TableRow.propTypes = {
