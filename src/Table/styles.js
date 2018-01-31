70572f69fdeaececa669014c855d573e8be03265
pbasti <p.bastidascarbonell@gmail.com>
2018-01-31 13:07:31 -0300

    style(table): style for header and dividers on table (#3)

--- src/Table/styles.js
+++ src/Table/styles.js
@@ -1,32 +1,19 @@
 import {StyleSheet} from 'react-native';
 
-const divider = {
-    flex: 1,
-    marginTop: 3,
-    marginRight: 10,
-    marginBottom: 3,
-    marginLeft: 10
-};
-
 export default StyleSheet.create({
     row: {
         marginTop: 20
     },
     col: {
-        paddingLeft: 20
-    },
-    headerContainer: {
-        flex: 0.2,
+        paddingLeft: 20,
         justifyContent: 'center',
+        alignItems: 'center'
+    },
+    tableHeader: {
+        flex: 0.05,
+        flexDirection: 'row',
         alignItems: 'center',
-        marginTop: 30
-    },
-    rowDivider: {
-        ...divider,
-        backgroundColor: '#dcdcdc'
-    },
-    headerDivider: {
-        ...divider,
-        backgroundColor: '#0e516e'
+        borderBottomWidth: 1,
+        borderBottomColor: '#b1b4b5'
     }
 });
