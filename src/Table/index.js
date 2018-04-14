55f5f748fbff9fdbe53efc2048d947040901d694
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2018-04-14 09:16:40 -0300

    feat(table): add functionality for order rows

--- src/Table/index.js
+++ src/Table/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {ScrollView} from 'react-native';
-import {isEmpty} from 'lodash';
+import {filter, isEmpty, map, sortBy} from 'lodash';
 import {Col, Row} from '@indec/react-native-commons';
 
 import columnPropType from '../util/columnPropType';
@@ -9,14 +9,20 @@ import datumPropType from '../util/datumPropType';
 import TableRow from './TableRow';
 import TableHeader from './TableHeader';
 
+const getData = (data, columns) => {
+    const fields = map(filter(columns, column => column.order), item => item.field);
+    return !isEmpty(fields) ? sortBy(data, fields) : data;
+};
+
 const Table = ({data, columns, striped}) => (
     <Col>
         <TableHeader columns={columns}/>
         <Row>
             <ScrollView>
-                {!isEmpty(data) && data.map((datum, index) => (
-                    <TableRow key={datum._id} striped={striped} rowIndex={index} datum={datum} columns={columns}/>
-                ))}
+                {!isEmpty(data) && getData(data, columns)
+                    .map((datum, index) => (
+                        <TableRow key={datum._id} striped={striped} rowIndex={index} datum={datum} columns={columns}/>
+                    ))}
             </ScrollView>
         </Row>
     </Col>
