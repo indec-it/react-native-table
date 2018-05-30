31b231f980def35bea83e553e31ea9ad8854687d
juanlopez1 <juan1manuel9lopez96@gmail.com>
2018-05-29 21:00:47 -0300

    fix(components): remove console warnings

--- src/Table/TableRow.js
+++ src/Table/TableRow.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Row} from '@indec/react-native-commons';
+import {mergeStyles} from '@indec/react-native-commons/util';
 
 import columnPropType from '../util/columnPropType';
 import TableCell from './TableCell';
@@ -13,7 +14,11 @@ const hasAccentStyle = (rowIndex, striped) => striped && rowIndex % 2 === 0;
 const TableRow = ({
     datum, columns, rowIndex, striped
 }) => (
-    <Row key={datum._id} style={[styles.row, hasAccentStyle(rowIndex, striped) && styles.rowEven]} size={0.1}>
+    <Row
+        key={datum._id}
+        style={mergeStyles(styles.row, hasAccentStyle(rowIndex, striped) && styles.rowEven)}
+        size={0.1}
+    >
         {columns.map(column => (
             <TableCell key={column.id} datum={datum} column={column}/>
         ))}
