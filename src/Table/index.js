5f7e2c7d701e25d2bbfffd448c0d54198e04eb58
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2019-01-07 23:13:14 -0300

    feat(table): add support for custom key field for table row

--- src/Table/index.js
+++ src/Table/index.js
@@ -10,13 +10,15 @@ import datumPropTypes from '../util/datumPropTypes';
 import TableRow from './TableRow';
 import TableHeader from './TableHeader';
 
-const Table = ({data, columns, striped}) => (
+const Table = ({
+    data, columns, striped, getKey
+}) => (
     <Col>
         <TableHeader columns={columns}/>
         <Row>
             <ScrollView>
                 {!isEmpty(data) && data.map((datum, index) => (
-                    <TableRow key={datum._id} striped={striped} rowIndex={index} datum={datum} columns={columns}/>
+                    <TableRow key={getKey(datum)} rowIndex={index} {...{striped, datum, columns}}/>
                 ))}
             </ScrollView>
         </Row>
@@ -26,11 +28,13 @@ const Table = ({data, columns, striped}) => (
 Table.propTypes = {
     columns: PropTypes.arrayOf(columnPropTypes).isRequired,
     data: PropTypes.arrayOf(datumPropTypes),
+    getKey: PropTypes.func,
     striped: PropTypes.bool
 };
 
 Table.defaultProps = {
     data: [],
+    getKey: datum => datum._id,
     striped: false
 };
 
